99339536394d474fc19f69e72088bf687400f72f
Andrey135296 <Andrey135296@yandex.ru>
2020-11-07 20:45:41 +0500

    even even more skeletons

---
+++ Mafia/Domain/City.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Messaging;
+
+namespace Mafia.Domain
+{
+    public class City : ICity
+    {
+        public ICollection<IPerson> Population { get; }
+        public ICollection<IPerson> Immortals { get; private set; }
+        public DayTime DayTime { get; private set; }
+        
+        public City(ICollection<IPerson> population)
+        {
+            Population = population;
+            DayTime = DayTime.Day;
+        }
+        
+        public void EndDay()
+        {
+            DayTime = DayTime.Night;
+        }
+
+        public void EndNight()
+        {
+            Immortals.Clear();
+            DayTime = DayTime.Day;
+        }
+
+        public void Kill(IPerson person)
+        {
+            if (!Population.Contains(person))
+            {
+                Console.Error.Write("Unknown Person");
+                return;
+            }
+
+            if (!Immortals.Contains(person))
+                person.Die();
+        }
+    }
+}
