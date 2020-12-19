5bdd45409157f7fb8185372882d8598af4a24a6f
krutovsky-danya <krutovsky.danya@gmail.com>
2020-12-19 22:25:00 +0500

    Little bot changes #1

--- Mafia/App/TGBot.cs
+++ Mafia/App/TGBot.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -6,12 +7,15 @@ using Mafia.Domain;
 using Telegram.Bot;
 using Telegram.Bot.Args;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
 namespace Mafia.App
 {
     public class TGBot : IUserInterface
     {
         private static TelegramBotClient Bot;
+
+        private static readonly ConcurrentDictionary<long, IPlayersPool> PlayersPools = new ConcurrentDictionary<long, IPlayersPool>();
         
         public IPerson AskForInteractionTarget(IEnumerable<IPerson> players, Role role, ICity city)
         {
@@ -44,7 +48,7 @@ namespace Mafia.App
             throw new NotImplementedException();
         }
 
-        static TGBot()
+        public TGBot()
         {
             var token = Environment.GetEnvironmentVariable("TGBotToken");
             // if (token == null)
@@ -59,6 +63,8 @@ namespace Mafia.App
             var me = Bot.GetMeAsync().Result;
 
             Bot.OnMessage += BotOnMessageReceived;
+            
+            Bot.StartReceiving(Array.Empty<UpdateType>());
         }
         
         // /startgame -> settings [defaultCondition, people]
@@ -66,56 +72,123 @@ namespace Mafia.App
             // all players must type /play, (randomly choose role)
             // if player is peaceful [nightRole is null]
         // finally create game and start it already
+        
         // more shit code ahead
 
-        private static void BotOnMessageReceived(object sender, MessageEventArgs messageEventArgs)
+        private static async void BotOnMessageReceived(object sender, MessageEventArgs messageEventArgs)
         {
             var message = messageEventArgs.Message;
 
             var chat = message.Chat;
             var user = message.From;
-            
 
-            // chat.id => game record
-            
-            if (message.Text == "//startgame")
+            await Bot.SendTextMessageAsync(chat.Id, $"Chat.Id: {chat.Id}\nUser: {user.Id}");
+
+            if (message.Text == "/startgame")
             {
-                // if game on record, pass
+                if (PlayersPools.ContainsKey(chat.Id))
+                {
+                    await Bot.SendTextMessageAsync(chat.Id, "Game record already has started!");
+                    return;
+                }
                 
-                // take chat.Id
-                // when in this chat someone types /play
-                // save person, chat id; give roles
+                var pool = new PlayersPool();
+                PlayersPools[chat.Id] = pool;
+                
+                await Bot.SendTextMessageAsync(chat.Id, "Record to game started!");
             }
 
-            if (message.Text == "//play")
+            if (message.Text == "/play")
             {
-                // if in chat.Id opened game record
-                    // if person not in this game, add to game
-            }
+                if (!PlayersPools.ContainsKey(chat.Id))
+                {
+                    await Bot.SendTextMessageAsync(chat.Id, "There no opened game record\nType /startgame to start one");
+                }
 
-            var msg = Bot.SendTextMessageAsync(chat.Id, "Cum");
+                var pool = PlayersPools[chat.Id];
+
+                if (!pool.IsOpen)
+                {
+                    await Bot.SendTextMessageAsync(chat.Id, "Sorry, record is end up");
+                    return;
+                }
+
+                var added = pool.AddPlayerAsync(user.Id);
+
+                if (added)
+                {
+                    await Bot.SendTextMessageAsync(chat.Id, "Successfully join game!");
+                }
+                else
+                {
+                    await Bot.SendTextMessageAsync(chat.Id, "Already joined game");
+                }
+            }
+            
+            // TODO: /endRecord -> form game -> start it
+            
+            // TODO: /help
+
+            var msg = Bot.SendTextMessageAsync(chat.Id, "Technical problems");
         }
     }
 
-    public interface IPlayersPool
+    public interface IPlayersPool // TODO: may be disposable
     {
-        bool IsOpen { get; set; }
-        Task AddPlayerAsync();
-        IEnumerable<Person> ExtractPersons();
+        bool IsOpen { get; }
+        bool AddPlayerAsync(long playerId);
+        IEnumerable<(IPerson, long)> ExtractPersons();
     }
 
     public class PlayersPool : IPlayersPool
     {
-        public bool IsOpen { get; set; }
+        private static readonly string[] Names = {
+            "Liam", "Olivia", "Noah", "Emma",
+            "Oliver", "Ava", "William", "Sophia",
+            "Elijah", "Isabella", "James", "Charlotte",
+            "Benjamin", "Amelia", "Lucas", "Mia",
+            "Mason", "Harper", "Ethan", "Evelyn"
+        };
+        
+        public bool IsOpen { get; private set; } = true;
+        
+        private readonly Random random = new Random();
+        private readonly ConcurrentDictionary<long, IPerson> players = new ConcurrentDictionary<long, IPerson>();
 
-        public async Task AddPlayerAsync()
+        public bool AddPlayerAsync(long playerId)
         {
-            throw new NotImplementedException();
+            if (players.ContainsKey(playerId))
+            {
+                return false;
+            }
+
+            var person = CreatePerson();
+            players[playerId] = person;
+            return true;
         }
 
-        public IEnumerable<Person> ExtractPersons()
+        private IPerson CreatePerson()
         {
-            throw new NotImplementedException();
+            var dayRole = new CitizenRole();
+            var index = players.Count % 3; // count of inheritors of Role
+            Role nightRole = index switch // TODO: make this cool
+            {
+                1 => new MafiaRole(),
+                2 => new HealerRole(),
+                _ => null
+            };
+
+            var name = Names[random.Next(0, Names.Length)];
+            return new Person(dayRole, nightRole, name);
+        }
+
+        public IEnumerable<(IPerson, long)> ExtractPersons() // TODO: redo
+        {
+            IsOpen = false;
+            foreach (var keyValue in players)
+            {
+                yield return (keyValue.Value, keyValue.Key);
+            }
         }
     }
 }
